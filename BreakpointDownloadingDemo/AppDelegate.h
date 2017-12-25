8ba018370ccc38c8af79756ea65ed72a82804235
macmini <macmini@macminidemac-mini-6.local>
2017-12-25 17:24:11 +0800

    Initial Commit

---
+++ BreakpointDownloadingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BreakpointDownloadingDemo
+//
+//  Created by macmini on 2017/12/25.
+//  Copyright © 2017年 macmini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BreakpointDownloadingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BreakpointDownloadingDemo
+//
+//  Created by macmini on 2017/12/25.
+//  Copyright © 2017年 macmini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
